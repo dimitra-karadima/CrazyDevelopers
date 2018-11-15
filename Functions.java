99fac2d5f9016561f5de3984e0f324d473fe470c
Stefanos Kypritidis <stef4k@gmail.com>
2018-11-15 02:09:43 +0200

    Fixed appearance and design

--- Functions.java
+++ Functions.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Functions {
 	static ArrayList<ArrayListLine> lists[]; // array that contains arraylists
+	private static Scanner input = new Scanner(System.in); // new Scanner object
 
 	/*
 	 * Constractor of our class Sets length of our array of arraylists
@@ -22,8 +23,25 @@ public class Functions {
 	/*
 	 * Method that adds a new object(line) to a specific list
 	 */
-	public void add(ArrayListLine object, int num) {
+	public void addLine(ArrayListLine object, int num) {
 		lists[num].add(object);
+	}
+
+	/*
+	 * Method that allows user to add the new line to a table Asks for the number of
+	 * table to add a new line and then asks for the Strings for every column After
+	 * it creates a new line using the addLine(...) method
+	 */
+	public void addChoice(int[] arrayNumberColumns) {
+		System.out.print("Enter number of table you wish to add a new object: ");
+		int tableAdd = input.nextInt();
+		String[] newObject = new String[arrayNumberColumns[tableAdd - 1]];
+		for (int i = 0; i < arrayNumberColumns[tableAdd - 1]; i++) {
+			System.out.print("Enter something for column " + (i + 1) + ": ");
+			newObject[i] = input.next();
+		}
+		ArrayListLine xx = new ArrayListLine(newObject);
+		addLine(xx, tableAdd - 1);
 	}
 
 	/*
@@ -34,12 +52,37 @@ public class Functions {
 	}
 
 	/*
+	 * Method that deletes a specific line of a specific table Asks user to input
+	 * number of table and number of line Uses method delete of class Functions to
+	 * do so
+	 */
+	public void deleteChoice() {
+		System.out.print("Select list to delete an item: ");
+		int list = input.nextInt();
+		System.out.print("Select number of item: ");
+		int item = input.nextInt();
+		delete(item - 1, list - 1);
+	}
+
+	/*
 	 * Method that prints a specifil object of a list
 	 */
 	public void print(int list, int num2) {
 		lists[list].get(num2).printLine();
-		;
+	}
 
+	/*
+	 * Method that prints all the lines and columns(all the objects ArrayListLine
+	 * there are) of the existing tables Uses the method print of Functions to do so
+	 */
+	public void printChoice(int numberOfTables) {
+		for (int i = 0; i < numberOfTables; i++) {
+			System.out.println("\tTable " + (i + 1));
+			for (int j = 0; j < lists[i].size(); j++) {
+				print(i, j);
+			}
+			System.out.println();
+		}
 	}
 
 	/*
@@ -47,5 +90,47 @@ public class Functions {
 	 */
 	public void change(int list, int line, int column, String change) {
 		lists[list].get(line).changeColumn(column, change);
+	}
+
+	/*
+	 * Method that allows a user to change a specific column of a specifil line of a
+	 * specific table Ask user to input number of table, number of line and number
+	 * of column This method uses the method change of class Functions to do so
+	 */
+	public void changeChoice() {
+		System.out.print("Select table to change an item from : ");
+		int table = input.nextInt();
+		System.out.print("Select the line of item: ");
+		int item = input.nextInt();
+		System.out.print("Select the column of the item: ");
+		int col = input.nextInt();
+		System.out.print("Enter new value: ");
+		String newValue = input.next();
+		change(table - 1, item - 1, col - 1, newValue);
+	}
+
+	/*
+	 * Method that prints the menu to the screen and returns the choice of the user
+	 */
+	public int choiceForFunctions() {
+		System.out.println("Enter: 1 to add, 2 to show, 3 to delete, 4 to change data");
+		int choice = input.nextInt();
+		return choice;
+
+	}
+
+	/*
+	 * Method to create an array with columns the number of columns of the specific
+	 * position of the table by asking user to enter positive numbers for example if
+	 * columnPerTable={3,5,1} means that table 1 has 3 columns , table 2 has 5
+	 * columns, table 3 has 1 column returns this table
+	 */
+	public int[] menuColumns(int tables) {
+		int[] columnPerTable = new int[tables];
+		for (int i = 0; i < tables; i++) {
+			System.out.print("Enter a positive amount of columns for table " + (i + 1) + ": ");
+			columnPerTable[i] = input.nextInt();
+		}
+		return columnPerTable;
 	}
 }
--- Main2.java
+++ Main2.java
@@ -8,73 +8,41 @@ public class Main2 {
 	private static Scanner input = new Scanner(System.in); // new Scanner object
 
 	public static void main(String[] args) {
-		int tables = openingMenu(); // User enters amount of tables for data base
-		Functions data = new Functions(tables); // new Functions object called data
-		int[] arrayNumberColumns = menuColumns(tables); // Creation of an array that contains the amount of columns for
-														// every table
+		int numTables = openingMenu(); // User enters amount of tables for data base
+		Functions data = new Functions(numTables); // new Functions object called data
+		int[] arrayNumberColumns = data.menuColumns(numTables);
+		// Creation of an array that contains the amount of columns for every table
+
 		for (;;) {
-			menuForFunctions(); // print menu
-			int choice = input.nextInt();
-			switch (choice) { // Condition
+			switch (data.choiceForFunctions()) { // Condition
 			case (1): { // case of adding a new object
-				System.out.print("Enter number of table you wish to add a new object: ");
-				int tableAdd = input.nextInt();
-				String[] newObject = new String[arrayNumberColumns[tableAdd - 1]];
-				for (int i = 0; i < arrayNumberColumns[tableAdd - 1]; i++) {
-					System.out.print("Enter something for column " + (i + 1) + ": ");
-					newObject[i] = input.next();
-				}
-				ArrayListLine xx = new ArrayListLine(newObject);
-				data.add(xx, tableAdd - 1);
+				data.addChoice(arrayNumberColumns);
 				break;
 			}
 			case (2): { // case of printing data
-				for (int i = 0; i < tables; i++) {
-					System.out.println("\tTable " + (i + 1));
-					for (int j = 0; j < data.lists[i].size(); j++) {
-						data.print(i, j);
-					}
-					System.out.println();
-				}
+				data.printChoice(numTables);
 				break;
 			}
 			case (3): { // case of deleting data
-				System.out.print("Select list to delete an item: ");
-				int list = input.nextInt();
-				System.out.print("Select number of item: ");
-				int item = input.nextInt();
-				data.delete(item - 1, list - 1);
+				data.deleteChoice();
 				break;
 
 			}
 			case (4): { // case of changind data
-				System.out.print("Select table to change an item from : ");
-				int table = input.nextInt();
-				System.out.print("Select the line of item: ");
-				int item = input.nextInt();
-				System.out.print("Select the column of the item: ");
-				int col = input.nextInt();
-				System.out.print("Enter new value: ");
-				String newValue = input.next();
-				data.change(table - 1, item - 1, col - 1, newValue);
+				data.changeChoice();
 				break;
 			}
 			default: {
 				System.out.println("Invalid choice");
 			}
 			}
-
 		}
-
 	}
 
 	/*
-	 * Method that prints the menu to the screen
+	 * Method that asks user to enter the number of tables for the database and
+	 * return this integer
 	 */
-	public static void menuForFunctions() {
-		System.out.println(" Enter: 1 to add, 2 to show, 3 to delete, 4 to change data");
-	}
-
 	public static int openingMenu() {
 		System.out.print("Enter amount of tables: "); // tables check
 		int tables = input.nextInt();
@@ -83,14 +51,5 @@ public class Main2 {
 			tables = input.nextInt();
 		}
 		return tables;
-	}
-
-	public static int[] menuColumns(int tables) {
-		int[] columnPerTable = new int[tables];
-		for (int i = 0; i < tables; i++) {
-			System.out.print("Enter a positive amount of columns for table " + (i + 1) + ": ");
-			columnPerTable[i] = input.nextInt();
-		}
-		return columnPerTable;
 	}
 }
