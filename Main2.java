0635edfa266196548e206ae5fcc5f042eda850f5
dimitra-karadima <mourtzakiiwanna@hotmail.com>
2018-12-26 20:15:18 +0200

    Fix classes using checkstyle

--- Main2.java
+++ Main2.java
@@ -1,87 +1,99 @@
+package dds.gr.aueb.dmst.crazydevelopers;
+
+import dds.gr.aueb.dmst.crazydevelopers.Functions;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 /*
  * Main method to run the program
  */
-import java.util.*;
-import java.io.*;
 
 public class Main2 {
-	private static Scanner input = new Scanner(System.in); // new Scanner object
-	static String testnames[][];
-	static String testnamesT[];
-	public static void main(String[] args) {
-		int exitCode = 0;
-		Menu menu = new Menu(); //new Menu object called menu
-		int numTables = menu.openingMenu(); // User enters amount of tables for data base
-		Functions data = new Functions(numTables); // new Functions object called data
-		menu.nameTables(numTables);
-		int[] arrayNumberColumns = menu.menuColumns(numTables);
-		// Creation of an array that contains the amount of columns for every table
-		testnames = menu.getNames();
-		testnamesT = menu.getNamesT();
-
-		while (exitCode == 0) { //loops until user chooses to save the DataBase to a file
-			switch (menu.choiceForFunctions()) { // Condition
-			case (1): { // case of adding a new object
-				data.addChoice(arrayNumberColumns,testnames);
-				break;
-			}
-			case (2): { // case of printing data
-				data.printChoice(testnamesT, testnames);
-				break;
-			}
-			case (3): { // case of deleting data
-				data.deleteChoice();
-				break;
-
-			}
-			case (4): { // case of changing data
-				data.changeChoice(arrayNumberColumns);
-				break;
-			}
-			case (5): { // case of ending loop and programm to save to file
-				int exitcode = 0;
-				while (exitcode == 0) {
-					try {
-						System.out.println("Enter the name of the file you want to save the database:");
-						String name = input.next();
-						int choice = menu.findChoice();
-						String path;
-						if (choice == 2) {
-							path = "C:\\Users/Public\\";
-							System.out.println("Congratulations! You can now find the database in " + path + name); //informs
-																													//the
-																													//user
-																													//where
-																													//to
-																													//find
-																													//the
-																													//file
-						} else {
-							path = menu.findPath();
-							System.out.println("Congratulations! You can now find the file in your pc with name " + name);
-							System.out.println("If you did not give full path you will find it in this java project folder with name " + path + name);
-							//if the user does now enter correct path programm use the String path as name of the file and saves
-							//it in the same folder with this java project
-						}
-						exitcode = 1;
-						FileWriter file = new FileWriter(path + name); // creates the file with name given
-																						// by the user
-						data.fileAdd(path, name, testnamesT, testnames);
-						file.close();	//releases useless resource
-					} catch (IOException e) {
-						System.out.println("Error!Wrong path given!");
-						exitcode = 0;
-					}
-				}
-				input.close();			//releases useless resource
-				exitCode = 1; // ends programm
-				break;
-			}
-			default: {
-				System.out.println("Invalid choice");
-			}
-			}
-		}
-	}
+  private static Scanner input = new Scanner(System.in); // new Scanner object
+  static String[][] testnames;
+  static String[] testnamesT;
+  /**
+   * Main method.
+   */
+  
+  public static void main(String[] args) {
+    Menu menu = new Menu(); //new Menu object called menu
+    int numTables = menu.openingMenu(); // User enters amount of tables for data base
+    Functions data = new Functions(numTables); // new Functions object called data
+    menu.nameTables(numTables);
+    int[] arrayNumberColumns = menu.menuColumns(numTables);
+    // Creation of an array that contains the amount of columns for every table
+    testnames = menu.getNames();
+    testnamesT = menu.getNamesT();
+    int exitCode = 0;
+    while (exitCode == 0) { //loops until user chooses to save the DataBase to a file
+      switch (menu.choiceForFunctions()) { // Condition
+        case (1): { // case of adding a new object
+          data.addChoice(arrayNumberColumns,testnames);
+          break;
+        }
+        case (2): { // case of printing data
+          data.printChoice(testnamesT, testnames);
+          break;
+        }
+        case (3): { // case of deleting data
+          data.deleteChoice();
+          break;
+        }
+        case (4): { // case of changing data
+          data.changeChoice(arrayNumberColumns);
+          break;
+        }
+        case (5): { // case of ending loop and programm to save to file
+          int exitcode = 0;
+          while (exitcode == 0) {
+            try {
+              System.out.println("Enter the name of the file you want to save the database:");
+              String name = input.next();
+              int choice = menu.findChoice();
+              String path;
+              if (choice == 2) {
+                path = "C:\\Users/Public\\";
+                System.out.println("Congratulations!You can now find the database in " 
+                    + path + name);//inform
+                //the
+                //user
+                //where
+                //to
+                //find
+                //the
+                //file
+              } else {
+                path = menu.findPath();
+                System.out.println("Congratulations! You can now find the file"
+                    + " in your pc with name " + name);
+                System.out.println("If you did not give full path you will find"
+                    + " it in this java project folder with name " + path + name);
+                //if the user does now enter correct path program use the String path as name 
+                //of the file and saves
+                //it in the same folder with this java project
+              }
+              exitcode = 1;
+              FileWriter file = new FileWriter(path + name); // creates the file with name given
+              // by the user
+              data.fileAdd(path, name, testnamesT, testnames);
+              file.close(); //releases useless resource
+            } catch (IOException e) {
+              System.out.println("Error!Wrong path given!");
+              exitcode = 0;
+            }
+          }
+          input.close(); //releases useless resource
+          exitCode = 1; // ends program
+          break;
+        }
+        default: {
+          System.out.println("Invalid choice");
+        }
+      }
+    }
+  }
 }
+
--- Menu.java
+++ Menu.java
@@ -1,124 +1,129 @@
+package dds.gr.aueb.dmst.crazydevelopers;
 
 /*
  * Class that contains all the menus needed for the program
  */
-import java.util.*;
+
+import java.util.Scanner;
+
 public class Menu {
-	Exceptions e = new Exceptions(); //new Exceptions object called e
-	private String names[][];
-	private String namesT[];
-	int columnPerTable[];
-	Scanner input = new Scanner(System.in);
+  Exceptions ex = new Exceptions(); //new Exceptions object called e
+  private String[][] names;
+  private String[] namesT;
+  int []columnPerTable;
+  Scanner input = new Scanner(System.in);
+  /**
+   * Method that asks user to enter the number of tables for the database and
+   * return this integer.
+   */
+  
+  public int openingMenu() {
+    ex.display("Enter amount of tables: "); // tables check
+    int tables = ex.notIntegerException();
+    while (tables <= 0) { //checks that amount of tables is bigger than 0 and not an int
+      ex.display("Enter positive quantity: ");
+      tables = ex.notIntegerException();
+    }
+    return tables;
+  }
+  /**
+   * Method that prints the menu to the screen and returns the choice of the user.
+   */
+  
+  public int choiceForFunctions() {
+    ex.display("Enter: 1 to add, 2 to show, 3 to delete, 4 to change data, "
+        + "5 to end programm and save it on a file");
+    int choice = ex.notIntegerException();// InputMisMatchException is only checked here
+    return choice;
+  }
 
-	/*
-	 * Method that asks user to enter the number of tables for the database and
-	 * return this integer
-	 */
-	public static int openingMenu() {
-		Exceptions e = new Exceptions();
-		e.display("Enter amount of tables: "); // tables check
-		int tables = e.NotIntegerException();
-		while (tables <= 0) {			//checks that amount of tables is bigger than 0 and not an integer
-			e.display("Enter positive quantity: ");
-			tables = e.NotIntegerException();
-		}
-		return tables;
-	}
+  /**
+   * Method to create an array with columns the number of columns of the specific
+   * position of the table by asking user to enter positive numbers for example if
+   * columnPerTable={3,5,1} means that table 1 has 3 columns , table 2 has 5
+   * columns, table 3 has 1 column returns this table.
+   */
+  public int[] menuColumns(int tables) {
+    columnPerTable = new int[tables];
+    for (int i = 0; i < tables; i++) {
+      ex.display("Enter a positive amount of columns for table " + (i + 1) + ": ");
+      int amount = ex.notIntegerException();
+      while (amount < 1) { //checks that amount is bigger than 0 and an integer
+        ex.display("Enter a positive amount of columns for table " + (i + 1) + ": ");
+        amount = ex.notIntegerException();
+      }
+      columnPerTable[i] = amount;
+    }
+    addNames(columnPerTable);
+    return columnPerTable;
+  }
 
-	/*
-	 * Method that prints the menu to the screen and returns the choice of the user
-	 */
-	public int choiceForFunctions() {
-		e.display("Enter: 1 to add, 2 to show, 3 to delete, 4 to change data, 5 to end programm and save it on a file");
-		int choice = e.NotIntegerException();// InputMisMatchException is only checked here
-		return choice;
-	}
+  /**
+   * Method to create an two-dimension array with lines the number of tables the
+   * user wants and columns the number of columns of the specific table. The user
+   * gives the names for each column of every table and the method inserts these names
+   * into the two-dimension array names[][]
+   */
+  public void addNames(int[] columnPerTable) {
+    names = new String[columnPerTable.length][];
+    for (int i = 0; i < columnPerTable.length; i++) {
+      names[i] = new String[columnPerTable[i]];
+      for (int j = 0; j < names[i].length; j++) {
+        System.out.print("Enter name for the #" + (i + 1) + " table and #" + (j + 1) + " column:");
+        names[i][j] = input.next();
+      }
+    }
+  }
 
-	/*
-	 * Method to create an array with columns the number of columns of the specific
-	 * position of the table by asking user to enter positive numbers for example if
-	 * columnPerTable={3,5,1} means that table 1 has 3 columns , table 2 has 5
-	 * columns, table 3 has 1 column returns this table
-	 */
-	public int[] menuColumns(int tables) {
-		columnPerTable = new int[tables];
-		for (int i = 0; i < tables; i++) {
-			e.display("Enter a positive amount of columns for table " + (i + 1) + ": ");
-			int amount=e.NotIntegerException();
-			while (amount<1) {						//checks that amount is bigger than 0 and an integer
-				e.display("Enter a positive amount of columns for table " + (i + 1) + ": ");
-				amount = e.NotIntegerException();
-			}
-			columnPerTable[i]=amount;
-		}
-		addNames(columnPerTable);
-		return columnPerTable;
-	}
+  /**
+   * Method to give user the opportunity to name every table of the DataBase
+   * The table names are saved in the one-dimension array namesT[].
+   */
+  public void nameTables(int tables) {
+    namesT = new String[tables];
+    for (int i = 0; i < tables; i++) {
+      System.out.println("Enter name for the #" + (i + 1) + " table:");
+      namesT[i] = input.next();
+    }
+  }
 
-	/*
-	 * Method to create an two-dimension array with lines the number of tables the
-	 * user wants and columns the number of columns of the specific table. The user
-	 * gives the names for each column of every table and the method inserts these names
-	 * into the tho-dimension array names[][]
-	 */
-	public void addNames(int[] columnPerTable) {
-		names = new String[columnPerTable.length][];
-		for (int i = 0; i < columnPerTable.length; i++) {
-			names[i] = new String[columnPerTable[i]];
-			for (int j = 0; j < names[i].length; j++) {
-				System.out.print("Enter name for the #" + (i + 1) + " table and #" + (j + 1) + " column:");
-				names[i][j] = input.next();
-			}
-		}
-	}
+  /**
+   * Method to declare if the user wants to save the file in a certain path
+   * or wants to use the default one (User press 1)
+   * The default path is C:\\Users/Public\\ and it is declared by us (User press 2)
+   * Returns the choice that the user makes,which is 1 or 2.
+   */
+  public int findChoice() {
+    ex.display("Enter 1 if you want it to enter the path you want to save the file in"
+         + " or 2 to save it to C:\\Users/Public\\ : ");
+    int choice = ex.notIntegerException();
+    while (choice < 1 || choice > 2) {
+      System.out.println("Please enter only 1 or 2!");
+      choice = ex.notIntegerException();
+    }
+    return choice;
+  }
 
-	/*
-	 * Method to give user the opportunity to name every table of the DataBase
-	 * The table names are saved in the one-dimension array namesT[]
-	 */
-	public void nameTables(int tables) {
-		namesT = new String[tables];
-		for (int i = 0; i < tables; i++) {
-			System.out.println("Enter name for the #" + (i + 1) + " table:");
-			namesT[i] = input.next();
-		}
-	}
+  /*
+   * Method used only if user chooses to enter his own path
+   * Returns path
+   */
+  public String findPath() {
+    System.out.print("Enter the path you want to save your file in: ");
+    return input.next();
+  }
 
-	/*
-	 * Method to declare if the user wants to save the file in a certain path
-	 * or wants to use the default one (User press 1)
-	 * The dafault path is C:\\Users/Public\\ and it is declared by us (User press 2)
-	 * Returns the choice that the user makes,which is 1 or 2
-	 */
-	public int findChoice() {
-		e.display("Enter 1 if you want it to enter the path you want to save the file in or 2 to save it to C:\\Users/Public\\ : ");
-		int choice = e.NotIntegerException();
-		while (choice < 1 || choice > 2) {
-			System.out.println("Please enter only 1 or 2!");
-			choice = e.NotIntegerException();
-		}
-		return choice;
-	}
+  /*
+   * Simple get methods for names[][] and namesT[]
+   * Will be used by Main2 class
+   * Useful for the addChoice() and printChoice()
+   */
+  public String[][] getNames() {
+    return names;
+  }
 
-	/*
-	 * Method used only if user chooses to enter his own path
-	 * Returns path
-	 */
-	public String findPath() {
-		System.out.print("Enter the path you want to save your file in: ");
-		return input.next();
-	}
-
-	/*
-	 * Simple get methods for names[][] and namesT[]
-	 * Will be used by Main2 class
-	 * Usefull for the addChoice() and printChoice()
-	 */
-	public String[][] getNames() {
-		return names;
-	}
-
-	public String[] getNamesT() {
-		return namesT;
-	}
+  public String[] getNamesT() {
+    return namesT;
+  }
 }
+
